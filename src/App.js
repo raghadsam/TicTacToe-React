7bd22561c406fc2961ad5d7117afb7765fc1c5b2
raghadsam <raghad66samrout@gmail.com>
2021-09-05 19:40:04 +0300

    size of squares

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Game from "./Components/Game";
 import Homepage from "./Components/Homepage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
